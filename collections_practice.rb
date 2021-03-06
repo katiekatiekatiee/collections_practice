4b4e9ed910a718f7777910c6d80ffb66d49a5c9e
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-05 20:12:58 -0500

    commit tree 6d0f08c35676116e7e9a168800c108ac23e3c8bc

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+#require "pry"
 
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort_by { |n| -n }
+end
+
+def sort_array_char_count(array)
+    array.sort_by { |string| string.size }
+end
+
+def swap_elements(array)
+   array[0], array[1], array[2] = array[0], array[2], array[1]
+
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    new_array = []
+    array.each do |name|
+        #binding.pry
+        name[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select { |letter| letter.start_with?("a") }
+end
+
+def sum_array(array)
+    array.inject(0) { |sum, n| sum + n } 
+end
+
+def add_s(array)
+    array.each_with_index do |word, index|
+        if index == 1
+            word = word
+        else
+            word << "s"
+        end
+    end
+end
